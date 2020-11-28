b49b31f9b020611744d43a4ff5c0d9a00f0559cc
jowens424 <jackowensii@gmail.com>
2020-11-28 12:19:53 -0500

    Restart Game and Fixed Bugs

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -14,7 +14,7 @@ $(document).keypress(function(){
       nextSequence();
       started = true;
   }
-  })
+});
 
 //* create function to identify the clicked button
 
@@ -22,8 +22,6 @@ $(".btn").click(function() {
   var userChosenColor = $(this).attr("id");
 
   userClickedPattern.push(userChosenColor);
-
-  console.log(userClickedPattern);
 
   playSound(userChosenColor);
 
@@ -38,30 +36,50 @@ $(".btn").click(function() {
 
 function checkAnswer(currentLevel) {
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-  console.log("success");
-} if (userClickedPattern.length === gamePattern.length){
-  console.log("success");
+ if (userClickedPattern.length === gamePattern.length){
+  setTimeout(function () {
+    nextSequence();
+  }, 1000);
+}
 } else {
-  console.log("Game Over");
+  playSound("wrong");
+  $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    startOver();
 }
 }
+//*trigger game over screen
+
 //*generate random selection, count level number
 
 function nextSequence() {
-  level ++;
-  $("level-title").text("Level " + level);
+  //*reset userClickPattern for next level. Level counter was not working, updated.
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
 var randomChosenColor = buttonColors[randomNumber];
-
 gamePattern.push(randomChosenColor);
 
-$("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
+//*eliminated last fadeOut as buttons would disappear on start of game
+$("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 playSound(randomChosenColor);
 }
 
+//*create function to simulate pressing of button
 
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+  }
 
 //*control sound for different variables
 
@@ -70,12 +88,9 @@ function playSound(name) {
   audio.play();
 }
 
-//*create function to simulate pressing of button
-
-function animatePress(currentColor) {
-  $("#" + currentColor).addClass("pressed");
-
-  setTimeout(function () {
-    $("#" + currentColor).removeClass("pressed");
-  }, 100);
-  }
+//* reset the game
+    function startOver() {
+      level = 0;
+      gamePattern = [];
+      started = false;
+    }
